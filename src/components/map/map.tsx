9c095d5005f4a134fba2d2366698269d0b2ead10
Yroslav.Shikhov <slaykin72@gmail.com>
2025-06-22 22:23:20 +0500

    Больше подробностей (часть 2)

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,32 +1,29 @@
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef } from 'react';
 import { City, Offers, IconProperties} from '../../types';
 import useMap from '../../hooks/useMap';
 import L from 'leaflet';
 import PinActive from'/img/pin-active.svg';
 import Pin from '/img/pin.svg';
-
 const ICON_PROPERTIES: IconProperties = {
   iconAnchor: [20, 40],
   iconSize: [40, 40],
 };
-
 type MapProps = {
   offers: Offers;
   city: City;
-  selectedCard: string;
+  selectedCard?: string;
 }
 
 export default function Map ({city, offers, selectedCard}: MapProps) {
   const mapRef = useRef(null);
   const map = useMap({mapRef, city});
-
+  const markersRef = useRef<L.Marker[]>([]);
 
   const defaultCustomIcon = L.icon({
     iconUrl: Pin,
     iconSize: ICON_PROPERTIES.iconSize,
     iconAnchor: ICON_PROPERTIES.iconAnchor,
   });
-
   const currentCustomIcon = L.icon({
     iconUrl: PinActive,
     iconSize: ICON_PROPERTIES.iconSize,
@@ -35,23 +32,27 @@ export default function Map ({city, offers, selectedCard}: MapProps) {
 
   useEffect(() => {
     if (map) {
+      markersRef.current.forEach((marker) => marker.remove());
+      markersRef.current = [];
+
       offers.forEach((offer) => {
-        L.marker({
+        const offerMarker = L.marker({
           lat: offer.coordinates.latitude,
           lng: offer.coordinates.longitude
         }, {
           icon: (offer.id === selectedCard) ? currentCustomIcon : defaultCustomIcon,
         }).addTo(map);
+
+        markersRef.current.push(offerMarker);
       });
     }
   }, [currentCustomIcon, defaultCustomIcon, map, offers, selectedCard]);
 
   return (
-    <section
-      className="cities__map map"
+    <div
       ref={mapRef}
       style={{height: '600px', width: `${100}%`}}
     >
-    </section>
+    </div>
   );
 }
--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -77,7 +77,9 @@ export default function MainScreen ({foundPlace, offers, city}: MainScreenProps)
               <HotelCardList offers={offers} setCurrentCard={setCurrentCard}/>
             </section>
             <div className="cities__right-section">
-              <Map city={city} offers={offers} selectedCard={currentCard}/>
+              <section className='cities__map map '>
+                <Map city={city} offers={offers} selectedCard={currentCard}/>
+              </section>
             </div>
           </div>
         </div>
