e7dfd5bf79069237f18ed6f31a703f5bd7c81f73
Yroslav.Shikhov <slaykin72@gmail.com>
2025-07-14 19:06:50 +0500

    7.14. Истина где-то на сервере

--- src/components/sort/sort-types.tsx
+++ src/components/sort/sort-types.tsx
@@ -1,17 +1,17 @@
 import classnames from 'classnames';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setSorting } from '../../store/action';
-import { getCurrentSort } from '../../store/reducer';
+import { SortTypes } from '../../types/models';
+import { getCurrentSort } from '../../store/slices/sorting-slice/sorting-reducer';
 
 type SortingTypesProps = {
-  sort: string;
+  sort: SortTypes;
 }
 
 export default function SortingTypes({sort}: SortingTypesProps) {
   const currentSort = useAppSelector(getCurrentSort);
   const dispatch = useAppDispatch();
   const handleClick = () => dispatch(setSorting(sort));
-
   return (
     <li className={classnames('places__option', {'places__option--active' : currentSort === sort})} onClick={handleClick} tabIndex={0}>{sort}</li>
   );
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,24 +1,22 @@
 import { AuthorizationStatus } from './constants';
-import { Offer, Offers, Result } from './types/models';
+import { AuthStatus, Offer, Offers } from './types/models';
 
-export const authorization = (auth: AuthorizationStatus, trueResult: Result, falseResult: Result) => {
+export const authorization = (auth: AuthStatus, trueResult: string, falseResult: string) => {
   if (auth === AuthorizationStatus.Auth) {
     return trueResult;
   }
   return falseResult;
 };
 export const getCurrentDate = (convertData: Date) => `${convertData.getFullYear()}-${convertData.getMonth() + 1}-${convertData.getDate()}`;
-
 export const getMonthAndYear = (convertData: Date) => `${convertData.toLocaleString('default', {month: 'long'})}-${convertData.getFullYear()}`;
-
 const sortingMethods = {
   lowToHighPriceSorting: (first: Offer, second: Offer) => first.price - second.price,
   highToLowPriceSorting: (first: Offer, second: Offer) => second.price - first.price,
-  highToLowRatingSorting: (first: Offer, second: Offer) => second.ratingValue - first.ratingValue,
+  highToLowRatingSorting: (first: Offer, second: Offer) => second.rating - first.rating,
 };
 
 export const sortingTypes = {
-  popularOffers: (offers: Offers, name: string) => offers.filter((offer) => offer.town === name),
+  popularOffers: (offers: Offers, name: string) => offers.filter((offer) => offer.city.name === name),
   priceLowToHighOffers: (offers: Offers) => offers.sort(sortingMethods.lowToHighPriceSorting),
   priceHighToLowOffers: (offers: Offers) => offers.sort(sortingMethods.highToLowPriceSorting),
   topRatingOffers: (offers: Offers) => offers.sort(sortingMethods.highToLowRatingSorting),
