039045c056a278309c8972ddb9b5d5451ff8f3ea
Yroslav.Shikhov <slaykin72@gmail.com>
2025-07-14 19:30:26 +0500

    7.15. Добро пожаловать, или посторонним вход воспрещён (часть 1)

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,13 +1,7 @@
-import { AuthorizationStatus } from './constants';
-import { AuthStatus, Offer, Offers } from './types/models';
+import { Offer, Offers } from './types/models';
 
-export const authorization = (auth: AuthStatus, trueResult: string, falseResult: string) => {
-  if (auth === AuthorizationStatus.Auth) {
-    return trueResult;
-  }
-  return falseResult;
-};
 export const getCurrentDate = (convertData: Date) => `${convertData.getFullYear()}-${convertData.getMonth() + 1}-${convertData.getDate()}`;
+
 export const getMonthAndYear = (convertData: Date) => `${convertData.toLocaleString('default', {month: 'long'})}-${convertData.getFullYear()}`;
 const sortingMethods = {
   lowToHighPriceSorting: (first: Offer, second: Offer) => first.price - second.price,
